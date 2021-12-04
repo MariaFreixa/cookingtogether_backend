e37fd1013f0afd0d96f28d187454e2bd501dca97
Maria Freixa Torres <mery.blood@gmail.com>
2021-12-04 19:10:06 +0100

    cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -14,12 +14,10 @@ class Cors
      * @return mixed
      */
     public function handle($request, Closure $next) {
-        // permite peticiones desde cualquier origen
-        header('Access-Control-Allow-Origin: *');
-        // permite peticiones con métodos GET, PUT, POST, DELETE y OPTIONS
+        header('Access-Control-Allow-Origin: *'); 
+        header("Access-Control-Allow-Credentials: true");
         header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
-        // permite los headers Content-Type y Authorization
-        header('Access-Control-Allow-Headers: Content-Type, Authorization');
-        return $next($request);
+        header('Access-Control-Max-Age: 1000');
+        header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
     }
 }
