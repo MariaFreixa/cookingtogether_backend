a864e7992b772752eb91cfefffcde3b79052e50e
Maria Freixa Torres <mery.blood@gmail.com>
2021-12-04 11:26:45 +0100

    update cors

--- app/Http/Middleware/CORS.php
+++ app/Http/Middleware/CORS.php
@@ -11,8 +11,7 @@ class CORS {
         // permite peticiones con métodos GET, PUT, POST, DELETE y OPTIONS
         header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
         // permite los headers Content-Type y Authorization
-        header('Access-Control-Allow-Headers: Content-Type, Authorization');
+        header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
         return $next($request);
     }
-
 }
