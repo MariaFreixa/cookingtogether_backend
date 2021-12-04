0404611a8f9cdd500312096b994715f3448048cf
Maria Freixa Torres <mery.blood@gmail.com>
2021-12-04 13:01:14 +0100

    prueba conf

--- app/Http/Middleware/CORS.php
+++ app/Http/Middleware/CORS.php
@@ -4,16 +4,17 @@ namespace App\Http\Middleware;
 use Closure;
 
 class CORS {
-    
-    public function handle($request, Closure $next) {
-        header('Access-Control-Allow-Origin: *');
-        header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-        header("Allow: GET, POST, OPTIONS, PUT, DELETE");
-        $method = $_SERVER['REQUEST_METHOD'];
-        if($method == "OPTIONS") {
-            die();
+
+    public function handle($request, Closure $next)
+    {
+        if ($request->isMethod('OPTIONS')){
+            $response = Response::make();
+        } else {
+            $response = $next($request);
         }
-        return $next($request);
+        return $response
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application');
     }
 }
