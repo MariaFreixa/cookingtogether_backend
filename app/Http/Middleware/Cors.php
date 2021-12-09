680ff6c6e983bbd78e84d9009e5a1d8327b5d071
Maria Freixa Torres <mery.blood@gmail.com>
2021-12-09 13:25:01 +0100

    cors conf

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -6,8 +6,7 @@ use Closure;
 class CORS {
     
     public function handle($request, Closure $next) {
-        // permite peticiones desde cualquier origen
-        header('Access-Control-Allow-Origin: *');
+
         // permite peticiones con métodos GET, PUT, POST, DELETE y OPTIONS
         header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
         // permite los headers Content-Type y Authorization
